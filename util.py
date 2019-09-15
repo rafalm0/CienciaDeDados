876e07872cbe36a6a0e9a1e97fbe39d98ce307ad
Rafael almeida <48065060+rafalm0@users.noreply.github.com>
2019-09-15 17:53:38 -0300

    Add files via upload

--- util.py
+++ util.py
@@ -23,14 +23,10 @@ def padronize_column(names_list, dataframes, colunabase, colunanova=None):
     return
 
 
-def pareamento(dataframebase, dataframepareando, colunas,pathbasesaida,pathpareandosaida, valor_match = 0.89):
+def pareamento(dataframebase, dataframepareando,outroDataframe, colunas,pathbasesaida,pathpareandosaida,outropath, valor_match = 0.89):
     dataframebase['KEY'] = reduce(lambda a, b: a+b, [dataframebase[coluna] for coluna in colunas])
     dataframepareando['KEY'] = reduce(lambda a, b: a + b, [dataframepareando[coluna] for coluna in colunas])
-    # id_unicos = []
-    # for i, line in dataframebase.iterrows():
-    #     id_unicos.append(i)
-    #     dataframebase['ID_unico'] = id_unicos
-    # new = id_unicos[-1]
+    outroDataframe['KEY'] = reduce(lambda a, b: a + b, [outroDataframe[coluna] for coluna in colunas])
     if not 'ID_UNICO' in dataframebase.columns:
         dataframebase['ID_UNICO'] = range(0, len(dataframebase))
     size = len(dataframebase)
@@ -50,15 +46,38 @@ def pareamento(dataframebase, dataframepareando, colunas,pathbasesaida,pathparea
             id_unicos.append(size)
             size += 1
         else:
-            g = sorted(matches[line2['ID']],key = lambda a:a[0])[0][1]
+            g = sorted(matches[line2['ID']], key = lambda a: a[0], reverse = True)[0][1]
             id_unicos.append(g)
             usados.append(g)
         if j/size*100 > perc:
             print(perc, '%')
             perc += 1
     dataframepareando['ID_UNICO'] = id_unicos
+
+
+    matches = {}
+    id_unicos = []
+    for j, line2 in outroDataframe.iterrows():
+        for i, line in dataframebase.iterrows():
+            jaro_value = distance.get_jaro_distance(line['KEY'], line2['KEY'])
+            if (jaro_value > valor_match) and (i not in usados):
+                if line2['ID'] not in matches:
+                    matches[line2['ID']] = []
+                matches[line2['ID']].append([jaro_value, line['ID_UNICO']])
+        if line2['ID'] not in matches.keys():
+            id_unicos.append(size)
+            size += 1
+        else:
+            g = sorted(matches[line2['ID']], key = lambda a: a[0], reverse = True)[0][1]
+            id_unicos.append(g)
+            usados.append(g)
+        if j/size*100 > perc:
+            print(perc, '%')
+            perc += 1
+    outroDataframe['ID_UNICO'] = id_unicos
     dataframepareando.to_csv(pathpareandosaida)
     dataframebase.to_csv(pathbasesaida)
+    outroDataframe.to_csv(outropath)
     return
 
 
